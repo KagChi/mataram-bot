6aad84504309d36bf4a4c0db57d7c0ccae2f1c07
カグチ <59391215+KagChi@users.noreply.github.com>
2021-09-04 16:13:46 +0700

    chore: add try catch

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -39,25 +39,29 @@ export class readyEvent extends Listener {
         const channel = this.container.client.channels.cache.get("881013594509160468") as TextBasedChannels;
         if (!channel?.isText()) return;
         setInterval(async () => {
-            const image = await oneCak.getFirstImage();
-            const { body } = await get(image.url).set("Referer", "https://1cak.com/").set("Referrer-Policy", "strict-origin-when-cross-origin")
-                .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-                .set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1");
-            channel.send({
-                embeds: [
-                    new MessageEmbed()
-                        .addField("Caption: ", image.title)
-                        .setImage("attachment://image.jpg")
-                        .setColor("BLURPLE")
-                ],
-                files: [
-                    new MessageAttachment(body, "image.jpg")
-                ]
-            }).then(x => {
-                for (const emote of emoji) {
-                    x.react(emote);
-                }
-            });
+            try {
+                const image = await oneCak.getFirstImage();
+                const { body } = await get(image.url).set("Referer", "https://1cak.com/").set("Referrer-Policy", "strict-origin-when-cross-origin")
+                    .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                    .set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1")
+                channel.send({
+                    embeds: [
+                        new MessageEmbed()
+                            .addField("Caption: ", image.title)
+                            .setImage("attachment://image.jpg")
+                            .setColor("BLURPLE")
+                    ],
+                    files: [
+                        new MessageAttachment(body, "image.jpg")
+                    ]
+                }).then(x => {
+                    for (const emote of emoji) {
+                        x.react(emote);
+                    }
+                });
+            } catch (e) {
+                this.container.logger.info(e)
+            }
         }, 60000).unref();
 
         return this.container.logger.info(chalk.green(`[CLIENT]: ${this.container.client.user?.username.toUpperCase()} CONNECTED TO DISCORD`));
